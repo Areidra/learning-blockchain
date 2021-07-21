11f6279e39eda8149f8b1b36f3d0d17a00f3aedd
Sara Durell <Areidra@users.noreply.github.com>
2021-07-21 16:57:32 -0700

    Adding search functions

--- dev/networknode.js
+++ dev/networknode.js
@@ -151,58 +151,84 @@ app.post('/register-nodes-bulk', function (req, res) {
   res.json({ note: 'Bulk registration successful.' });
 });
 
-app.get('/consensus', function (req, res) {
+app.get('/consensus', function(req, res) {
+  const requestPromises = [];
   bitcoin.networkNodes.forEach(networkNodeUrl => {
     const requestOptions = {
       uri: networkNodeUrl + '/blockchain',
       method: 'GET',
       json: true
     };
-    requestPromise.push(rp(requestOptions));
+    requestPromises.push(rp(requestOptions));
   });
 
-  {
-    "chain": [
-      {
-        "index": 1,
-        "timestamp": 1626817271226,
-        "transactions": [],
-        "nonce": 100,
-        "hash": "0",
-        "previousBlockHash": "0"
-      },
-      {
-        "index": 2,
-        "timestamp": 1626817294678,
-        "transactions": [],
-        "nonce": 18140,
-        "hash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100",
-        "previousBlockHash": "0"
-      },
-      {
-        "index": 3,
-        "timestamp": 1626817299042,
-        "transactions": [
-          {
-            "amount": 12.5,
-            "sender": "00",
-            "recipient": "2546696faf5c40be9e2a630610b5f906",
-            "transactionId": "0efda1b7d8ca4e7caa697e21bb4c2b7e"
-          }
-        ],
-        "nonce": 11933,
-        "hash": "00003d11dfa1da66ff5ce7691e8af4cedfd6da6aa34946c14343d580d452ea8c",
-        "previousBlockHash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100"
-      }
-    ],
-      "pendingTransactions": [
-        {
-          "amount": 12.5,
-          "sender": "00",
-          "recipient": "2546696faf5c40be9e2a630610b5f906",
-          "transactionId": "929e4322101d4fb4b03919f946f6f4ad"
+    Promise.all(requestPromises)
+      .then(blockchains => {
+        const currentChainLength = bitcoin.chain.length;
+        let maxChainLength = currentChainLength;
+        let newLongestChain = null;
+        let newPendingTransactions = null;
+
+        blockchains.forEach(blockchain => {
+          if (blockchain.chain.length > maxChainLength) {
+            maxChainLength = blockchain.chain.length;
+            newLongestChain = blockchain.chain;
+            newPendingTransactions = blockchain.pendingTransactions;
+          };
+        });
+
+        if (!newLongestChain || (newLongestChain && !bitcoin.chainIsValid(newLongestChain))) {
+          res.json({
+            note: 'Current chain has not been replaced.',
+            chain: bitcoin.chain
+          });
         }
-      ],
-        "currentNodeUrl": "http://localhost:3001",
-          "networkNodes": []
-  }
+        else { //(newLongestChain && bitcoin.chainIsValid(newLongestChain)) {
+          bitcoin.chain = newLongestChain;
+          bitcoin.pendingTransactions = newPendingTransactions;
+          res.json({
+            note: 'This chain has been replaced.',
+            chain: bitcoin.chain
+          });
+        }
+
+
+      });
+  });
+
+
+  app.get('/block/:blockHash', function(req, res){
+    const blockHash = req.params.blockHash;
+    const correctBlock = bitcoin.getBlock(blockHash);
+    res.json({
+      block: correctBlock
+    })
+
+  });
+
+
+  app.get('/transaction/:transactionId', function(req, res){
+    const transactionId = req.params.transactionId;
+    const transactionData = bitcoin.getTransaction(transactionId);
+    res.json({
+      transaction: transactionData.transaction,
+      block: transactionData.block
+    })
+
+  });
+
+
+  app.get('/address/:address', function(req, res){
+    const address = req.params.address;
+    const addressData = bitcoin.getAddressData(address);
+    res.json({
+      addressData: addressData
+    });
+
+  });
+
+
+
+  app.listen(port, function() {
+    console.log(`Listening on port ${port}...`);
+  });
--- dev/test.js
+++ dev/test.js
@@ -4,49 +4,49 @@ const bitcoin = new Blockchain();
 const bc1 =
 {
     "chain": [
-    {
-    "index": 1,
-    "timestamp": 1626817271226,
-    "transactions": [],
-    "nonce": 100,
-    "hash": "0",
-    "previousBlockHash": "0"
-    },
-    {
-    "index": 2,
-    "timestamp": 1626817294678,
-    "transactions": [],
-    "nonce": 18140,
-    "hash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100",
-    "previousBlockHash": "0"
-    },
-    {
-    "index": 3,
-    "timestamp": 1626817299042,
-    "transactions": [
-    {
-    "amount": 12.5,
-    "sender": "00",
-    "recipient": "2546696faf5c40be9e2a630610b5f906",
-    "transactionId": "0efda1b7d8ca4e7caa697e21bb4c2b7e"
-    }
-    ],
-    "nonce": 11933,
-    "hash": "00003d11dfa1da66ff5ce7691e8af4cedfd6da6aa34946c14343d580d452ea8c",
-    "previousBlockHash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100"
-    }
+        {
+            "index": 1,
+            "timestamp": 1626817271226,
+            "transactions": [],
+            "nonce": 100,
+            "hash": "0",
+            "previousBlockHash": "0"
+        },
+        {
+            "index": 2,
+            "timestamp": 1626817294678,
+            "transactions": [],
+            "nonce": 18140,
+            "hash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100",
+            "previousBlockHash": "0"
+        },
+        {
+            "index": 3,
+            "timestamp": 1626817299042,
+            "transactions": [
+                {
+                    "amount": 12.5,
+                    "sender": "00",
+                    "recipient": "2546696faf5c40be9e2a630610b5f906",
+                    "transactionId": "0efda1b7d8ca4e7caa697e21bb4c2b7e"
+                }
+            ],
+            "nonce": 11933,
+            "hash": "00003d11dfa1da66ff5ce7691e8af4cedfd6da6aa34946c14343d580d452ea8c",
+            "previousBlockHash": "0000b9135b054d1131392c9eb9d03b0111d4b516824a03c35639e12858912100"
+        }
     ],
     "pendingTransactions": [
-    {
-    "amount": 12.5,
-    "sender": "00",
-    "recipient": "2546696faf5c40be9e2a630610b5f906",
-    "transactionId": "929e4322101d4fb4b03919f946f6f4ad"
-    }
+        {
+            "amount": 12.5,
+            "sender": "00",
+            "recipient": "2546696faf5c40be9e2a630610b5f906",
+            "transactionId": "929e4322101d4fb4b03919f946f6f4ad"
+        }
     ],
     "currentNodeUrl": "http://localhost:3001",
     "networkNodes": []
-    };
-        
+};
+
 
 console.log('VALID:', bitcoin.chainIsValid(bc1.chain));
